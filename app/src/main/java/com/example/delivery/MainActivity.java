2a214571807af816c59c82d0f24c87e056163c89
admin <ahmed.g@live.com>
2023-04-12 00:24:16 +0300

    new

--- app/src/main/java/com/example/delivery/MainActivity.java
+++ app/src/main/java/com/example/delivery/MainActivity.java
@@ -2,13 +2,100 @@ package com.example.delivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+    EditText user, password;
+    String username, pass, type;
+    Spinner types;
+    String URL = Server.ip + "login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        user = findViewById(R.id.log_user);
+        password = findViewById(R.id.log_pass);
+        types = findViewById(R.id.log_type);
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        user.setText("");
+        password.setText("");
+    }
+
+    public void logClick(View v) {
+        if (v.getId() == R.id.log_log) {
+            username = user.getText().toString().trim();
+            pass = password.getText().toString().trim();
+            type = types.getSelectedItem().toString().toLowerCase();
+            if (username.length() > 3) {
+                login();
+            } else {
+                Toast.makeText(this, getResources().getString(R.string.loginError), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void login() {
+        class RegAsync extends AsyncTask<String, Void, String> {
+            private Dialog loadingDialog;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loadingDialog = ProgressDialog.show(MainActivity.this, getResources().getString(R.string.wait), getResources().getString(R.string.connecting));
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                Connection con = new Connection();
+                HashMap<String, String> data = new HashMap<>();
+                data.put("username", params[0]);
+                data.put("password", params[1]);
+                data.put("type", type);
+                String result = con.sendPostRequest(URL, data);
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                loadingDialog.dismiss();
+                if (result.isEmpty() || result.equals("Error"))
+                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.check), Toast.LENGTH_LONG).show();
+                else if (result.equals("failure")) {
+                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.userExist), Toast.LENGTH_LONG).show();
+                }else if (result.equals("block")) {
+                    Toast.makeText(getApplicationContext(), "This Account is blocked", Toast.LENGTH_LONG).show();
+                } else if (result.equalsIgnoreCase("customer")) {
+                    Intent intent = new Intent(MainActivity.this, CustomerPage.class);
+                    intent.putExtra("id", username);
+                    startActivity(intent);
+                } else if (result.equalsIgnoreCase("staff")) {
+                    Intent intent = new Intent(MainActivity.this, StaffPage.class);
+                    intent.putExtra("id", username);
+                    startActivity(intent);
+                } else if (result.equalsIgnoreCase("agent")) {
+                    Intent intent = new Intent(MainActivity.this, AgentPage.class);
+                    intent.putExtra("id", username);
+                    startActivity(intent);
+                }
+            }
+        }
+        RegAsync la = new RegAsync();
+        la.execute(username, pass);
+    }
+
 }
--- app/src/main/java/com/example/delivery/Register.java
+++ app/src/main/java/com/example/delivery/Register.java
@@ -2,13 +2,206 @@ package com.example.delivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class Register extends AppCompatActivity {
+
+    EditText userN, pass, pass2, perName,  phone, email;
+    TextView l1, l2, l3, l4, l6, l7;
+
+    String user, password, name,  thePhone, Email;
+    String URL = Server.ip + "register.php";
+    Button reg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        userN = findViewById(R.id.reg_username);
+        pass = findViewById(R.id.reg_pass);
+        pass2 = findViewById(R.id.reg_pass2);
+        perName = findViewById(R.id.reg_name);
+        phone = findViewById(R.id.reg_phone);
+        email = findViewById(R.id.reg_email);
+
+
+        l1 = findViewById(R.id.l1);
+        l2 = findViewById(R.id.l2);
+        l3 = findViewById(R.id.l3);
+        l4 = findViewById(R.id.l4);
+        l6 = findViewById(R.id.l6);
+        l7 = findViewById(R.id.l7);
+
+
+        reg = findViewById(R.id.reg_reg);
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RegOnclick(view);
+            }
+        });
+
+
+    }
+
+    public void RegOnclick(View view) {
+        if (view.getId() == R.id.reg_reg) {
+
+            l1.setText(getResources().getString(R.string.username));
+            l1.setTextColor(Color.BLACK);
+            l2.setText(getResources().getString(R.string.password));
+            l2.setTextColor(Color.BLACK);
+            l3.setText(getResources().getString(R.string.conform_pasword));
+            l3.setTextColor(Color.BLACK);
+            l4.setText(getResources().getString(R.string.pername));
+            l4.setTextColor(Color.BLACK);
+
+            l6.setText(getResources().getString(R.string.phone));
+            l6.setTextColor(Color.BLACK);
+            l7.setText(getResources().getString(R.string.email));
+            l7.setTextColor(Color.BLACK);
+
+            user = userN.getText().toString().trim();
+            password = pass.getText().toString().trim();
+            String password2 = pass2.getText().toString().trim();
+            name = perName.getText().toString().trim();
+
+            thePhone = phone.getText().toString().trim();
+            Email = email.getText().toString().trim();
+
+            boolean error = false;
+            if (user.length() < 3) {
+                error = true;
+                String temp = l1.getText().toString();
+                l1.setText(temp + " *");
+                l1.setTextColor(Color.RED);
+                Toast.makeText(this, getResources().getString(R.string.enterUsername), Toast.LENGTH_LONG).show();
+            }
+            if (password.length() < 8) {
+                error = true;
+                String temp = l2.getText().toString();
+                l2.setText(temp + " *");
+                l2.setTextColor(Color.RED);
+                Toast.makeText(this, getResources().getString(R.string.enterPass), Toast.LENGTH_LONG).show();
+            }
+            if (!isValidPassword(password)) {
+                error = true;
+                String temp = l2.getText().toString();
+                l2.setText(temp + " *");
+                l2.setTextColor(Color.RED);
+                Toast.makeText(this, "The password must contain letters and numbers", Toast.LENGTH_LONG).show();
+            }
+            if (!password.equals(password2)) {
+                error = true;
+                String temp = l2.getText().toString();
+                l2.setText(temp + " *");
+                l2.setTextColor(Color.RED);
+                temp = l3.getText().toString();
+                l3.setText(temp + " *");
+                l3.setTextColor(Color.RED);
+                Toast.makeText(this, getResources().getString(R.string.enterPass2), Toast.LENGTH_LONG).show();
+            }
+            if (name.length() < 2) {
+                error = true;
+                String temp = l4.getText().toString();
+                l4.setText(temp + " *");
+                l4.setTextColor(Color.RED);
+                Toast.makeText(this, getResources().getString(R.string.enterPersonal), Toast.LENGTH_LONG).show();
+            }
+
+            if (!isValidEmail(Email)) {
+                error = true;
+                String temp = l7.getText().toString();
+                l7.setText(temp + " *");
+                l7.setTextColor(Color.RED);
+                Toast.makeText(this, getResources().getString(R.string.enterEmail), Toast.LENGTH_LONG).show();
+            }
+            if (thePhone.length() != 10 || !thePhone.startsWith("05")) {
+                error = true;
+                String temp = l6.getText().toString();
+                l6.setText(temp + " *");
+                l6.setTextColor(Color.RED);
+                Toast.makeText(this, getResources().getString(R.string.enterPhone), Toast.LENGTH_LONG).show();
+            }
+
+            if (!error) {
+                Registration();
+            }
+
+        }
+    }
+
+    private void Registration() {
+        class RegAsync extends AsyncTask<String, Void, String> {
+            private Dialog loadingDialog;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loadingDialog = ProgressDialog.show(Register.this, getResources().getString(R.string.wait), getResources().getString(R.string.connecting));
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+                Connection con = new Connection();
+                HashMap<String, String> data = new HashMap<>();
+                data.put("username", params[0]);
+                data.put("password", params[1]);
+                data.put("name", params[2]);
+                data.put("phone", params[3]);
+                data.put("email", params[4]);
+
+
+                String result = con.sendPostRequest(URL, data);
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                loadingDialog.dismiss();
+                if (result.isEmpty() || result.equals("Error"))
+                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.check), Toast.LENGTH_LONG).show();
+                else if (result.equals("failure")) {
+                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.noResult), Toast.LENGTH_LONG).show();
+                } else if (result.equalsIgnoreCase("Exist")) {
+                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.enterREgistration), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.success), Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(Register.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }
+        RegAsync la = new RegAsync();
+        la.execute(user, password, name, thePhone, Email);
+    }
+
+    public boolean isValidEmail(CharSequence target) {
+        if (TextUtils.isEmpty(target)) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
+    }
+
+    public boolean isValidPassword(String s) {
+        String n = ".*[0-9].*";
+        String a = ".*[A-Z].*";
+        String sa = ".*[a-z].*";
+        return (s.matches(n) && s.matches(a)) || (s.matches(n) && s.matches(sa));
     }
 }
