5f074ee2b328872223260170306eab56b963d87d
admin <ahmed.g@live.com>
2023-05-15 18:14:39 +0300

    new

--- app/src/main/java/com/example/delivery/AgentPage.java
+++ app/src/main/java/com/example/delivery/AgentPage.java
@@ -2,13 +2,32 @@ package com.example.delivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AgentPage extends AppCompatActivity {
-
+    String id;
+    Button orders;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agent_page);
+        orders=findViewById(R.id.orders);
+
+        id=getIntent().getStringExtra("id");
+
+
+        orders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AgentPage.this,OrderList.class);
+                intent.putExtra("id",id);
+                intent.putExtra("type","delivery agent");
+                startActivity(intent);
+            }
+        });
+
     }
 }
